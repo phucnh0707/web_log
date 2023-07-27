38696b86cc9ada85a014483be697345671dadf1b
phuc0707 <nugyenhphuc0707@gmail.com>
2023-07-27 22:02:47 +0700

    v1.1.0

---
+++ web-log/resources/views/pages/blogs.blade.php
@@ -0,0 +1,6 @@
+@extends('../layout')
+@section('content')
+    
+<p>Tất cả bài viết</p>
+{{-- <a href="{{route('bolgs',['id'=>5])}}">Xem chi tiết bài</a> --}}
+@endsection
---
+++ web-log/resources/views/pages/detail.blade.php
@@ -0,0 +1,5 @@
+@extends('../layout')
+@section('content')
+<p>Tiêu đề: Du lịch Hạ Long</p>
+<p>Bài viết</p>
+@endsection
